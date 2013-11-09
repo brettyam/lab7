cc4e7123102623be62b10943ca3c578a1867a6bf
Brett Yamada <brett.yamada@gmail.com>
2013-11-08 20:43:20 -0800

    Lab 7 complete with optional steps

--- js/earthquakes.js
+++ js/earthquakes.js
@@ -24,6 +24,57 @@ gov.usgs.quakes;
 //reference to our google map
 gov.usgs.quakesMap;
 
+//document on ready
+$(function(){
+	$('.message').html('Loading... <img src="img/loading.gif">');
+	getQuakes();
+});
+
+//queries the server for the list of recent quakes
+//and plots them on a Google map
+function getQuakes() {
+	$.getJSON(gov.usgs.quakesUrl, function(quakes){
+		//set global variable to current set of quakes
+		//to be referenced later in another event
+		gov.usgs.quakes = quakes;
+		$('.message').html('Displaying ' + quakes.length + ' earthquakes');
+		gov.usgs.quakesMap = new google.maps.Map($('.map-container')[0], {
+			center: new google.maps.LatLng(0, 0),
+			zoom: 2,
+			mapTypeId: google.maps.MapTypeId.TERRAIN,
+			streetViewControl: false
+		}); //create map
+		addQuakeMarkers(quakes, gov.usgs.quakesMap);
+	}); //handle returned data function
+} //getQuakes()
+
+function addQuakeMarkers(quakes, map) {
+	var quake;	//current quake data
+	var idx;	//loop counter
+
+	//loop over the quakes array and add a marker for each one
+	for (idx = 0; idx < quakes.length; ++idx) {
+		quake = quakes[idx];
+		quake.mapMarker = new google.maps.Marker({
+			map: map,
+			position: new google.maps.LatLng(quake.location.latitude, quake.location.longitude)
+		});
+		google.maps.event.addListener(quake.mapMarker, 'click', function(){
+			// if an info window is already open, close it
+			if (gov.usgs.iw) {
+				gov.usgs.iw.close();
+			}
+			//create an info window with the quake info
+			gov.usgs.iw = new google.maps.InfoWindow({
+    			content: new Date(quake.datetime).toLocaleString() + ': magnitude ' + quake.magnitude 
+    			+ ' at depth of ' + quake.depth + ' meters'
+			});
+
+			gov.usgs.iw.open(map, this); //open the info window
+		}); //click handler for marker
+	} //loop over quakes array
+} //addQuakeMarkers()
+
 //AJAX Error event handler
 //just alerts the user of the error
 $(document).ajaxError(function(event, jqXHR, err){
